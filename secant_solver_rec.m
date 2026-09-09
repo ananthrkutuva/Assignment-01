9128448fbb69161241d2ae0aeb76e20c91190734
Ananth Kutuva <ananthkutuva@gmail.com>
2026-09-09 15:55:18 -0400

    added desc above secant solver rec

--- secant_solver_rec.m
+++ secant_solver_rec.m
@@ -1,3 +1,17 @@
+%Root finding function via secant method
+%INPUTS:
+% fun: the function we are computing the root of
+% x0: first guess for secant method
+% x1: second guess for secant method
+% dxtol: termination threshold (stop when interval abs(x_{i+1}-x_i) < dxtol)
+% ftol: termination threshold (stop when abs(f(x_{i}))<ftol
+% max_iter: maximum iteration limit
+% dxmax: threshold for checking for a divide by zero error:
+% terminate when abs(x_{i+1}-x_i) > dxmax, where dxmax is a very large number
+%OUTPUTS
+% x: estimate for root of fun
+% exit_flag: an integer indicating whether or not the solver succeeded
+% x_list: a list of that iterations [x0 x1 xn]
 function [x_final, exit_flag, x_list] = secant_solver_rec(fun, x0, x1, dxtol, ftol, max_iter, dxmax)
     max_iter = max_iter - 1;
     
