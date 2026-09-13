5c7584348b513aac95d861d38e5804ffb3fe4cd2
Ananth Kutuva <ananthkutuva@gmail.com>
2026-09-13 17:13:52 -0400

    working on secant method sigmoid convergence plot

--- secant_sigmoid_convergence_plot.m
+++ secant_sigmoid_convergence_plot.m
@@ -1,32 +1,20 @@
-function newton_sigmoid_convergence_plot()
+function secant_sigmoid_convergence_plot()
     close all;
     %Initial guess near the root we are analyzing convergence behavior
     %(you will need to change this depending on the test function and root)
     x0_ref = 26;
     target_root = fzero(@test_function03,x0_ref);
-
+    
     % solver parameters
     dxtol = 1e-14;
-    ftol = 1e-12;
+    ftol = 1e-14;
     max_iter = 10000;
     dxmax = 1e7; %Newton and Secant only
     
-    %Create an instance of the input_recorder
-    my_recorder = input_recorder();
-    
-    %Use input_recorder to generate a version of the test function
-    %that records the input after every iteration
-    %Since test_fun is defined using function keyword
-    f_record = my_recorder.generate_recorder_fun(@test_function03);
-    
-    %number of trials we would like to perform
-    num_iter = 200;
-    
-    %list for the initial guesses that we would like
-    %to use each trial. These guesses have all been chosen
-    %so that each trial will converge to the same root
-    %because the root is somewhere between-5 and 5.
-    x0_list = linspace(0, 50, num_iter);
+    % setting number of iterations for the sweep and the sweep bounds
+    num_iter = 1000;
+    x0_list = linspace(0, 26, num_iter);
+    x1_list = linspace(26.2, 50, num_iter);
     
     %list of estimate at current iteration (x_{n})
     %compiled across all trials
@@ -42,73 +30,55 @@ function newton_sigmoid_convergence_plot()
 
     % list of exit flags
     exit_flag_list = [];
-
-    % list of function outputs
-    % fun_outputs = [];
     
-    %loop through each trial
     for n = 1:num_iter
-        %pull out the guess for the trial
+        % sets the first x0 and x1 to use for that iteration
         x0 = x0_list(n);
-    
-        %reset input_list for the next test
-        my_recorder.clear_input_list();
-    
-        %Call your root finder using the recording function:
-        [~, exit_flag] = newton_solver(f_record, x0, dxtol, ftol, max_iter, dxmax);
-    
-        %See what input values were used when f_record was called:
-        input_list = my_recorder.get_input_list();
-    
-        %at this point, input_list will be populated with the values that
-        %the solver called at each iteration.
-        %In other words, it is now [x_1,x_2,...x_n-1,x_n]
-        %append the collected data to the compilation
+        x1 = x1_list(n);
         
+        % runs the solver
+        [~, exit_flag, x_list] = secant_solver_rec(@test_function03, x0, x1, dxtol, ftol, max_iter, dxmax);
+        
+        % adds the current x, next x and its index to their lists as well
+        % as the newest exit flag
+        x_current_list = [x_current_list, x_list(1:end-1)];
+        x_next_list = [x_next_list, x_list(2:end)];
+        index_list = [index_list, 1:length(x_list)-1];
         exit_flag_list = [exit_flag_list, exit_flag];
-        x_current_list = [x_current_list,input_list(1:end-1)];
-        x_next_list = [x_next_list,input_list(2:end)];
-        index_list = [index_list,1:length(input_list)-1];
-        % fun_outputs = [fun_outputs, x_next_list(end)];
     end
     
-    %At this point, x_current_list corresponds to many many
-    %measurements of x_{n} across many trials
-    %and x_next_list corresponds to many many measurements of
-    %the corresponding value of x_{n+1} across many trials
-    %this is the data the you want to clean and analyze
-      
-    % current and next error lists
+    % calculating current and next error
     e_n = abs(x_current_list - target_root);
     e_n1 = abs(x_next_list - target_root);
     
     % trim boundaries
-    xmin = 2.06399e-6;
-    xmax = 0.0189298;
+    xmin = 1.4e-8;
+    xmax = 0.011;
     
     % mask to remove all values outside boundary
     mask = (e_n >= xmin) & (e_n <= xmax);
     
-    % new trimmed error values
+    % new trimmed error values to use for fit line
     e_n_new = e_n(mask);
     e_n1_new = e_n1(mask);
     
-    % plotting the error values
-    loglog(e_n, e_n1,'ro','markerfacecolor','r','markersize',5);
+    % plotting the full secant method plot
+    figure;
+    loglog(e_n, e_n1, 'r.', 'MarkerSize', 10, 'DisplayName', 'Secant Method Raw Error');
     hold on;
     
-    % generating and plotting the fit line
+    % plotting the fit line on top of the plot
     [p, k] = generate_error_fit(e_n_new, e_n1_new);
-    fit_line_x = 10.^(-6:.01:-1);
+    fit_line_x = 10.^(-8:.01:-1);
     fit_line_y = k * fit_line_x .^ p;
-    loglog(fit_line_x,fit_line_y,'k-','linewidth', 3)
+    loglog(fit_line_x,fit_line_y,'k-','linewidth', 3, 'DisplayName', 'Filtered Convergence Fit Line')
     hold off;
     ax = gca;
-    ax.FontSize = 30; % Changes tick labels and scales labels
-    title("Newton's Method Convergence Rate Plot")
+    ax.FontSize = 30;
+    title("Secant Method Convergence Rate Plot")
     xlabel("\epsilon_{n} (-)")
     ylabel("\epsilon_{n+1} (-)")
-    legend("Newton's Method Raw Error", "Filtered Convergence Fit Line", location="southeast")
+    legend(location="southeast")
     
     % plotting the new sigmoid test function
     figure; hold on;
@@ -117,7 +87,7 @@ function newton_sigmoid_convergence_plot()
     xvals = linspace(-50, 50, 201);
     [yvals,~] = test_function03(xvals);
     plot(xvals, yvals,'k','linewidth', 4, "DisplayName", "Sigmoid Function");
-    xlabel('Function Input: x'); ylabel('Function Output: f(x)'); title("Newton's Method Guess Convergence Plot");
+    xlabel('Function Input: x'); ylabel('Function Output: f(x)'); title("Secant Method Guess Convergence Plot");
     xlim([0 50])
     ylim([-8 8])
     legend(location="southeast");
